baad14fe7d1e545ccf14d22babc55510cb555b32
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-08-21 15:28:16 +0700

    ContainerApp

--- app/src/main/java/com/example/roomsiswa/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/roomsiswa/repositori/ContainerApp.kt
@@ -1,2 +1,14 @@
 package com.example.roomsiswa.repositori
 
+import android.content.Context
+import com.example.roomsiswa.room.DatabaseSiswa
+
+interface ContainerApp{
+    val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context): ContainerApp{
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(DatabaseSiswa.getDatabase(context).siswaDao())
+    }
+}

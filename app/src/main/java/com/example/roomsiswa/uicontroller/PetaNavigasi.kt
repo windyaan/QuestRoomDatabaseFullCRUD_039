2d667c458775e85d1033e41d6c4095f0675a2301
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-08-21 18:46:45 +0700

    fun HostNavigasi

--- app/src/main/java/com/example/roomsiswa/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/roomsiswa/uicontroller/PetaNavigasi.kt
@@ -1,5 +1,9 @@
 package com.example.roomsiswa.uicontroller
 
+import com.example.roomsiswa.view.DestinasiEntry
+import com.example.roomsiswa.view.DestinasiHome
+import com.example.roomsiswa.view.EntrySiswaScreen
+import com.example.roomsiswa.view.HomeScreen
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
@@ -7,6 +11,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.roomsiswa.R
 
@@ -40,3 +46,25 @@ fun SiswaTopAppBar(
     )
 }
 
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)}
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(
+                navigateBack = { navController.popBackStack() }
+            )
+        }
+    }
+}

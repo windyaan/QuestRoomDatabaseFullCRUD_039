373f76a579580ed2711a0a2d7b4c535f47beab21
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-08-21 18:48:35 +0700

    import

--- app/src/main/java/com/example/roomsiswa/view/HalamanHome.kt
+++ app/src/main/java/com/example/roomsiswa/view/HalamanHome.kt
@@ -23,6 +23,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
@@ -34,6 +35,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.roomsiswa.R
 import com.example.roomsiswa.room.Siswa
 import com.example.roomsiswa.uicontroller.DestinasiNavigasi
+import com.example.roomsiswa.uicontroller.SiswaTopAppBar
 import com.example.roomsiswa.viewmodel.HomeViewModel
 import com.example.roomsiswa.viewmodel.PenyediaViewModel
 

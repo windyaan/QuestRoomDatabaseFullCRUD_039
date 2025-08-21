f4cad1773349431b950ca18fb3e206157f9fa5b1
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-08-21 15:20:12 +0700

    tambah data class Siswa

---
+++ app/src/main/java/com/example/roomsiswa/room/Siswa.kt
@@ -0,0 +1,13 @@
+package com.example.roomsiswa.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tblSiswa")
+data class Siswa(
+    @PrimaryKey(autoGenerate = true)
+    val id : Int = 0,
+    val nama : String,
+    val alamat : String,
+    val telepon : String
+)

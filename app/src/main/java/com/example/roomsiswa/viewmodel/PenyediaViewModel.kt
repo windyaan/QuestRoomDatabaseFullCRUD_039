04284be955e31a401969fd30ab4fdf4d6854615f
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-08-21 16:22:08 +0700

    PenyediaViewModel

--- app/src/main/java/com/example/roomsiswa/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/viewmodel/PenyediaViewModel.kt
@@ -1,2 +1,22 @@
 package com.example.roomsiswa.viewmodel
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.roomsiswa.AplikasiSiswa
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
+//fungsi ekstensi query untuk objek
+fun CreationExtras.aplikasiSiswa():AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)

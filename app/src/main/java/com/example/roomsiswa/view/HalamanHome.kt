80f51a93735b2dc76705e0318b908bc869296172
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-08-21 16:42:35 +0700

    HalamanHome

--- app/src/main/java/com/example/roomsiswa/view/HalamanHome.kt
+++ app/src/main/java/com/example/roomsiswa/view/HalamanHome.kt
@@ -1,2 +1,59 @@
 package com.example.roomsiswa.view
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.roomsiswa.R
+import com.example.roomsiswa.uicontroller.DestinasiNavigasi
+import com.example.roomsiswa.viewmodel.HomeViewModel
+import com.example.roomsiswa.viewmodel.PenyediaViewModel
+
+object DestinasiHome : DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
+
+@OptIn
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+     modifier: Modifier,
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiHome.titleRes),
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add
+                )
+            }
+        }
+    ){  }
+}

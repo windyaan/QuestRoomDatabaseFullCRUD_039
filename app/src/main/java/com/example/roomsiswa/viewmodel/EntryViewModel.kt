27d51632be5174899578eb803263694fa9182041
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-08-21 15:54:30 +0700

    membuat var uiStateSiswa

--- app/src/main/java/com/example/roomsiswa/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/viewmodel/EntryViewModel.kt
@@ -1,2 +1,13 @@
 package com.example.roomsiswa.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.roomsiswa.repositori.RepositoriSiswa
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+   //berisi status siswa saat ini
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+       private set
+
+
+}

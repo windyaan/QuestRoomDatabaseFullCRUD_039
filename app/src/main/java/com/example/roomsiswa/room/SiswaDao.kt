67195a7966c01589a282d6e905a7c6344a48d5d6
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-08-21 15:21:18 +0700

    tambah interface SiswaDao

---
+++ app/src/main/java/com/example/roomsiswa/room/SiswaDao.kt
@@ -0,0 +1,17 @@
+package com.example.roomsiswa.room
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+// kelas abstract- tanpa implmeentasi(isi perintah)
+interface SiswaDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa: Siswa)
+
+    @Query("SELECT * from to tblSiswa ORDER BY nama ASC")
+    fun getAllSiswa():Flow<List<Siswa>>
+}

f1993dcee10d9b94d6375606b72e56609f7735e4
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-08-26 18:24:44 +0700

    tambah fun EditSiswaScreen

--- app/src/main/java/com/example/roomsiswa/view/HalamanEdit.kt
+++ app/src/main/java/com/example/roomsiswa/view/HalamanEdit.kt
@@ -1,11 +1,28 @@
 package com.example.roomsiswa.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.roomsiswa.R
 import com.example.roomsiswa.uicontroller.DestinasiNavigasi
+import com.example.roomsiswa.viewmodel.EditViewModel
+import com.example.roomsiswa.viewmodel.PenyediaViewModel
 
 object DestinasiEditSiswa : DestinasiNavigasi{
     override val route = "item_edit"
     override val tittleRes = R.string.edit_siswa
     const val itemIdArg = "idSiswa"
     val routeWithArgs = "$route/{$itemIdArg}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
 }

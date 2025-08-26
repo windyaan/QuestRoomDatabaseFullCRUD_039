1a1a5b85f5aa93c15259ee61bba9d2818384d8f1
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-08-26 18:22:11 +0700

    tambah object

--- app/src/main/java/com/example/roomsiswa/view/HalamanEdit.kt
+++ app/src/main/java/com/example/roomsiswa/view/HalamanEdit.kt
@@ -1,2 +1,11 @@
 package com.example.roomsiswa.view
 
+import com.example.roomsiswa.R
+import com.example.roomsiswa.uicontroller.DestinasiNavigasi
+
+object DestinasiEditSiswa : DestinasiNavigasi{
+    override val route = "item_edit"
+    override val tittleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}

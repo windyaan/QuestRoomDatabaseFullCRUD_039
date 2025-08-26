769bd99fc5a6cb8acd35354e160cc0e168a9509f
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-08-26 15:43:36 +0700

    edit RepositoriSiswa.kt

---
+++ app/src/main/java/com/example/roomsiswa/viewmodel/DetailViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.roomsiswa.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.roomsiswa.repositori.RepositoriSiswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class DetailViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoriSiswa: RepositoriSiswa) : ViewModel() {
+
+        private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetailSiswa.itemIdArg])
+
+        val uiDetailState: StateFlow<DetailSiswaUiState> =
+        repositoriSiswa.getSiswaStream(idSiswa)
+            .filterNotNull()
+            .map {
+                DetailSiswaUiState(detailSiswa = it.toSDetailSiswa())
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = DetailSiswaUiState()
+            )
+
+        suspend fun deleteSiswa(){
+            repositoriSiswa.deleteSiswa(uiDetailState.value.detailSiswa.toSiswa())
+        }
+
+        companion object{
+            private const val TIMEOUT_MILLIS = 5_000L
+        }
+    }
+
+data class DetailSiswaUiState(
+    val detailSiswa: DetailSiswa = DetailSiswa()
+)

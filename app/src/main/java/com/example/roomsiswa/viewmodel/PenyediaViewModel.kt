a9fc7c56a021aecdcc15f63244f7966ab24c4ff8
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-08-26 15:43:57 +0700

    edit PenyediaViewModel

--- app/src/main/java/com/example/roomsiswa/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.roomsiswa.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -13,6 +14,16 @@ object PenyediaViewModel {
         }
         initializer {
             EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+
+        //edit : tambah initializer Detail dan Edit
+        initializer {
+            DetailViewModel(this.createSavedStateHandle(),
+                aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EditViewModel(this.createSavedStateHandle(),
+                aplikasiSiswa().container.repositoriSiswa)
         }
     }
 }

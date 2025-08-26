065f0187f65fd2dd598ce70f2a9797d456311591
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-08-26 16:00:15 +0700

    tambah EditViewModel

---
+++ app/src/main/java/com/example/roomsiswa/viewmodel/EditViewModel.kt
@@ -0,0 +1,54 @@
+package com.example.roomsiswa.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.roomsiswa.repositori.RepositoriSiswa
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class EditViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+
+        var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle
+    [DestinasiDetailSiswa.itemIdArg])
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoriSiswa.getSiswaStream(idSiswa)
+                .filterNotNull()
+                .first()
+                .toUiStateSiswa(true)
+        }
+
+        class EditViewModel(
+            fun updateUiState(detailSiswa: DetailSiswa){
+            uiStateSiswa =
+                UIStateSiswa(detailSiswa = DetailSiswa, isEntryValid =
+                validasiInput(detailSiswa))
+        }
+
+        private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa)
+        :Boolean{
+            return with(uiState){
+                nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+            }
+        }
+
+        suspend fun updateSiswa(){
+            if (validasiInput(uiStateSiswa.detailSiswa)){
+                repositoriSiswa.updateSiswa(
+                    uiStateSiswa.detailSiswa.toSiswa()
+                )
+            }
+        }
+    }
+}
+

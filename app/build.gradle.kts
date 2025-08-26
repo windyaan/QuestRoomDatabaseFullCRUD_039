3ab223a6d43c124cff50375f216d6df02a014e09
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-08-26 15:05:22 +0700

    tambah App

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     defaultConfig {
         applicationId = "com.example.roomsiswa"
         minSdk = 30
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 

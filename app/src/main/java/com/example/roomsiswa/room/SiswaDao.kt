bd0e6c2f9f5df794b41531e4d0712c4ab3134cf4
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-08-26 15:17:00 +0700

    edit SiswaDao

--- app/src/main/java/com/example/roomsiswa/room/SiswaDao.kt
+++ app/src/main/java/com/example/roomsiswa/room/SiswaDao.kt
@@ -1,9 +1,11 @@
 package com.example.roomsiswa.room
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -14,4 +16,14 @@ interface SiswaDao {
 
     @Query("SELECT * from tblSiswa ORDER BY nama ASC")
     fun getAllSiswa():Flow<List<Siswa>>
+
+    //Edit : tambah 3 fungsi
+    @Query("SELECT * from tblSiswa WHERE id = :id")
+    fun getSiswa(id: Int):Flow<Siswa>
+
+    @Update
+    suspend fun update(siswa: Siswa)
+
+    @Delete
+    suspend fun delete(siswa: Siswa)
 }

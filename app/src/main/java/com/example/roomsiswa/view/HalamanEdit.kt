94a3ce5bed416ac12a7c47e09c23aaa01183ef70
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-08-26 18:31:43 +0700

    HalamanEdit.kt complete

--- app/src/main/java/com/example/roomsiswa/view/HalamanEdit.kt
+++ app/src/main/java/com/example/roomsiswa/view/HalamanEdit.kt
@@ -1,13 +1,20 @@
 package com.example.roomsiswa.view
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.roomsiswa.R
 import com.example.roomsiswa.uicontroller.DestinasiNavigasi
+import com.example.roomsiswa.uicontroller.SiswaTopAppBar
 import com.example.roomsiswa.viewmodel.EditViewModel
 import com.example.roomsiswa.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
 
 object DestinasiEditSiswa : DestinasiNavigasi{
     override val route = "item_edit"
@@ -24,5 +31,28 @@ fun EditSiswaScreen(
     modifier: Modifier = Modifier,
     viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
-
+    Scaffold (
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEditSiswa.tittleRes),
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
+            )
+        },
+        modifier = modifier
+    ) {
+        innerPadding ->
+        val coroutineScope = rememberCoroutineScope()
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updateSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
 }

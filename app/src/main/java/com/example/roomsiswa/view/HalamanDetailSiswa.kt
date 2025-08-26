c0b4edba2dc476da1b7c889d435db2535579ce35
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-08-26 18:02:41 +0700

    tambah fun DetailDataSiswa

--- app/src/main/java/com/example/roomsiswa/view/HalamanDetailSiswa.kt
+++ app/src/main/java/com/example/roomsiswa/view/HalamanDetailSiswa.kt
@@ -1,32 +1,53 @@
 package com.example.roomsiswa.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.room.Delete
 import com.example.roomsiswa.R
+import com.example.roomsiswa.room.Siswa
 import com.example.roomsiswa.uicontroller.DestinasiNavigasi
 import com.example.roomsiswa.uicontroller.SiswaTopAppBar
+import com.example.roomsiswa.viewmodel.DetailSiswa
+import com.example.roomsiswa.viewmodel.DetailSiswaUiState
 import com.example.roomsiswa.viewmodel.DetailViewModel
 import com.example.roomsiswa.viewmodel.PenyediaViewModel
+import com.example.roomsiswa.viewmodel.toSiswa
+import kotlinx.coroutines.launch
 
 object DestinasiDetailSiswa :DestinasiNavigasi{
     override val route = "detail_siswa"
 
-    override val tittleRes = "Detail Siswa"
+    override val tittleRes = R.string.detail_siswa
 
-    const val itemArg = "idSiswa"
+    const val itemIdArg = "idSiswa"
 
     val routeWithArgs = "$route/{$itemIdArg}"
 }
@@ -68,6 +89,80 @@ fun DetailSiswaScreen (
         }, modifier = modifier
     ){ innerPaddng ->
         val uiState = viewModel.uiDetailState.collectAsState()
-        va
+        val coroutineScope = rememberCoroutineScope()
+        BodyDetailDataSiswa(
+            detailSiswaUiState = uiState.value,
+            onDelete = {coroutineScope.launch {
+                viewModel.deleteSiswa()
+                navigateBack()
+            }},
+            modifier = Modifier
+                .padding(innerPaddng)
+                .verticalScroll(rememberScrollState())
+        )
+    }
+}
+
+@Composable
+private fun BodyDetailDataSiswa(
+    detailSiswaUiState: DetailSiswaUiState,
+    onDelete: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Column (
+        modifier = modifier.padding(dimensionResource(R.dimen.padding_medium)),
+        verticalArrangement = Arrangement.spacedBy(
+            dimensionResource(R.dimen.padding_medium)
+        )
+    ){
+        var deleteConfirmationRequired by rememberSaveable {
+            mutableStateOf(false)
+        }
+
+        DetailDataSiswa(
+            siswa = detailSiswaUiState.detailSiswa.toSiswa(),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedButton(
+            onClick = {deleteConfirmationRequired = true},
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.delete))
+        }
+        if (deleteConfirmationRequired){
+            DeleteConfirmationDialog(
+                onDeleteConfirm = {
+                    deleteConfirmationRequired = false
+                    onDelete()
+                },
+                onDeleteCancel = {deleteConfirmationRequired = false},
+                modifier = Modifier.padding(dimensionResource(
+                    id = R.dimen.padding_medium
+                ))
+            )
+        }
+    }
+}
+
+@Composable
+fun DetailDataSiswa(
+    siswa: Siswa, modifier: Modifier = Modifier
+){
+    Card (
+        modifier = modifier, colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ){
+        Column (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(R.dimen.padding_medium)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium))
+        ){
+
+        }
     }
 }
